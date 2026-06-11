3c466e6ef51bcbc789747800693fc6afc612081e
HiddenHAlly <alessandro.a.t.turco@gmail.com>
2026-06-11 20:25:22 +0000

    Version 1.5.1 1.21.11, added creative inventory support and fabric loader >=0.19.2 support.

--- src/main/java/net/hiddenhally/buganair/BuganairMod.java
+++ src/main/java/net/hiddenhally/buganair/BuganairMod.java
@@ -1,6 +1,7 @@
 package net.hiddenhally.buganair;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.hiddenhally.buganair.Buganair;
@@ -12,6 +13,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -19,6 +22,7 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class BuganairMod implements ModInitializer {
@@ -69,16 +73,42 @@ public class BuganairMod implements ModInitializer {
     }
 
     // All variants registered with string keys perfectly matching your asset paths
-    public static final Item BUGANAIR_OAK_BOAT = registerBoatItem("oak");
-    public static final Item BUGANAIR_PALE_OAK_BOAT = registerBoatItem("pale_oak");
-    public static final Item BUGANAIR_SPRUCE_BOAT = registerBoatItem("spruce");
-    public static final Item BUGANAIR_BIRCH_BOAT = registerBoatItem("birch");
-    public static final Item BUGANAIR_JUNGLE_BOAT = registerBoatItem("jungle");
-    public static final Item BUGANAIR_ACACIA_BOAT = registerBoatItem("acacia");
-    public static final Item BUGANAIR_DARK_OAK_BOAT = registerBoatItem("dark_oak");
-    public static final Item BUGANAIR_MANGROVE_BOAT = registerBoatItem("mangrove");
-    public static final Item BUGANAIR_CHERRY_BOAT = registerBoatItem("cherry");
-    public static final Item BUGANAIR_BAMBOO_RAFT = registerBoatItem("bamboo");
+    public static final Item BUGANAIR_OAK_BOAT_ITEM = registerBoatItem("oak");
+    public static final Item BUGANAIR_PALE_OAK_BOAT_ITEM = registerBoatItem("pale_oak");
+    public static final Item BUGANAIR_SPRUCE_BOAT_ITEM = registerBoatItem("spruce");
+    public static final Item BUGANAIR_BIRCH_BOAT_ITEM = registerBoatItem("birch");
+    public static final Item BUGANAIR_JUNGLE_BOAT_ITEM = registerBoatItem("jungle");
+    public static final Item BUGANAIR_ACACIA_BOAT_ITEM = registerBoatItem("acacia");
+    public static final Item BUGANAIR_DARK_OAK_BOAT_ITEM = registerBoatItem("dark_oak");
+    public static final Item BUGANAIR_MANGROVE_BOAT_ITEM = registerBoatItem("mangrove");
+    public static final Item BUGANAIR_CHERRY_BOAT_ITEM = registerBoatItem("cherry");
+    public static final Item BUGANAIR_BAMBOO_RAFT_ITEM = registerBoatItem("bamboo");
+
+    // 1. Create the Registry Key for your custom tab
+    public static final RegistryKey<ItemGroup> BUGANAIR_ITEM_GROUP_KEY = RegistryKey.of(
+            RegistryKeys.ITEM_GROUP,
+            Identifier.of(Buganair.MOD_ID, "buganair_group")
+    );
+
+    // 2. Build the ItemGroup instance
+    public static final ItemGroup BUGANAIR_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(BuganairMod.BUGANAIR_OAK_BOAT_ITEM))
+            // 1. FIXED: Changed .title() to .displayName()
+            .displayName(Text.translatable("itemGroup.buganair.buganair_group"))
+            .entries((displayContext, entries) -> {
+                // 2. FIXED: Appended .getDefaultStack() to turn the raw Items into ItemStacks
+                entries.add(BuganairMod.BUGANAIR_ACACIA_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_BAMBOO_RAFT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_BIRCH_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_CHERRY_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_DARK_OAK_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_JUNGLE_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_MANGROVE_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_OAK_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_PALE_OAK_BOAT_ITEM.getDefaultStack());
+                entries.add(BuganairMod.BUGANAIR_SPRUCE_BOAT_ITEM.getDefaultStack());
+            })
+            .build();
 
     @Override
     public void onInitialize() {
@@ -91,7 +121,8 @@ public class BuganairMod implements ModInitializer {
                 boat.setSpeedSettings(payload.horizontalSpeed(), payload.verticalSpeed());
             }
         }));
-
+        // 4. Register the group inside your onInitialize method
+        Registry.register(Registries.ITEM_GROUP, BUGANAIR_ITEM_GROUP_KEY, BUGANAIR_ITEM_GROUP);
 
 
         Buganair.LOGGER.info("Mod {} initialized", Buganair.MOD_ID);
